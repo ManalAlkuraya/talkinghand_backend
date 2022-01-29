f75cc176343c0bb76555ff92e6a24b992c86a4af
ManalAlkuraya <92294514+ManalAlkuraya@users.noreply.github.com>
2022-01-29 23:11:58 +0300

    Update ServController.java

--- src/main/java/com/talking/hand/talkinghand/controller/ServController.java
+++ src/main/java/com/talking/hand/talkinghand/controller/ServController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "https://talkinghandbackend.herokuapp.com/")
+@CrossOrigin(origins = "https://talkinghandfrontend.herokuapp.com/")
 @RestController
 @RequestMapping(path="api/serv")
 
